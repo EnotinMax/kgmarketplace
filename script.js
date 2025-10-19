c5041090b6627966c62ac565315da9383392ce59
EnotinTECH <96776414+EnotinMax@users.noreply.github.com>
2025-10-19 23:41:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ class DialogueEditor {
         
         // Конец стрелки в центр левой границы целевого узла
         const toX = toNode.x;
-        const toY = toNode.y + (toNode.collapsed ? 21px);
+        const toY = toNode.y + (toNode.collapsed ? 25 : 75);
         
         line.setAttribute('x1', fromX);
         line.setAttribute('y1', fromY);
